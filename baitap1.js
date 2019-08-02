52ac9feca3f1d737f2bab352f148764e3b412799
trananhtuan27 <trananhtuan2710201@gmail.com>
2019-08-02 09:01:30 +0700

    bai thuc hanh

---
+++ baitap1.js
@@ -0,0 +1,8 @@
+let inputwith;
+let inputheight;
+inputwith = prompt("chieu rong")
+inputheight = prompt("chieu dai")
+let width = Number(inputwith);
+let height = Number(inputheight);
+let area = width * height;
+    document.write('ket qua la :'+ area);
